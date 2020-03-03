3a2873691013c07d4cb88c9c1abd98eec13e38d6
unknown <pedro.vicente.chitay135@gmail.com>
2020-03-02 20:13:20 -0600

    Version 1.1

---
+++ src/ejemplo/arreglo/EjemploArreglo.java
@@ -0,0 +1,26 @@
+
+package ejemplo.arreglo;
+
+public class EjemploArreglo {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        String[] arrCadena = new String[4];
+       arrCadena[0] = "rojo";
+       arrCadena[1] = "azul";
+       arrCadena[2] = "amarillo";
+       arrCadena[3] = "verde";
+        for (int i =0;   i<4; i++){
+            System.out.println(arrCadena[i]);
+        }
+       
+
+
+
+
+
+
+    }
+    
+}
